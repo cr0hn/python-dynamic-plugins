364b2c175440adcca5250356702fa2336f5077a7
cr0hn <cr0hn@cr0hn.com>
2022-08-02 13:14:15 +0200

    add - custom exceptions imp - add function for loading modules

--- dynamic_plugins/__init__.py
+++ dynamic_plugins/__init__.py
@@ -1,2 +1,3 @@
+from .exceptions import *
 from .extensions import *
 from .dependencies import *
--- dynamic_plugins/extensions.py
+++ dynamic_plugins/extensions.py
@@ -5,7 +5,36 @@ import sysconfig
 from types import ModuleType
 from typing import Tuple, List, Dict
 
+from .exceptions import DynamicPluginException
 from .dependencies import list_installed_packages
+
+def get_symbols_from_package(package: str, symbols: List[str] | str = None) -> Dict[str, dict] | DynamicPluginException:
+    """
+    This function dynamically loads all installed extensions and returns the functions and variables listed in symbols.
+
+    :return: list of functions and variables
+    """
+
+    symbols = symbols or []
+
+    if type(symbols) is str:
+        symbols = [symbols]
+
+    if type(package) is str:
+        try:
+            new_module = importlib.import_module(package)
+        except ModuleNotFoundError as e:
+            raise DynamicPluginException(f"Can't load module: {package}") from e
+    else:
+        new_module = package
+
+    ret_symbols = {}
+
+    for symbol in symbols:
+        ret_symbols[symbol] = getattr(new_module, symbol, None)
+
+    return ret_symbols
+
 
 def get_extensions(extensions_prefix: str = None, *, sub_package: str = None, symbols: List[str] | str = None) -> Dict[str, dict]:
     """
@@ -25,16 +54,11 @@ def get_extensions(extensions_prefix: str = None, *, sub_package: str = None, sy
     for module_name, module in get_packages(extensions_prefix):
 
         if sub_package:
-            new_module = importlib.import_module(f"{module_name}.{sub_package}")
+            new_module = f"{module_name}.{sub_package}"
         else:
             new_module = module
 
-        ret_symbols = {}
-
-        for symbol in symbols:
-            ret_symbols[symbol] = getattr(new_module, symbol, None)
-
-        ret[module_name] = ret_symbols
+        ret[module_name] = get_symbols_from_package(new_module, symbols)
 
     return ret
 
@@ -86,8 +110,11 @@ def get_packages(module_prefix: str = None) -> List[Tuple[str, ModuleType]]:
         if not module_name:
             continue
 
-        ret.append((module_name, importlib.import_module(module_name)))
+        try:
+            ret.append((module_name, importlib.import_module(module_name)))
+        except ModuleNotFoundError as e:
+            raise DynamicPluginException(f"Can't load module: {module_name}") from e
 
     return ret
 
-__all__ = ("get_packages", "get_extensions")
+__all__ = ("get_packages", "get_extensions", "get_symbols_from_package")
